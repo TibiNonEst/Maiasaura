596546c34d30ef931f3e3c0cfb88c6278285034a
tibs <git@tibinonest.me>
2022-07-04 11:06:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Maiasaura
+A maven repository built on Deno and stored Bunny.net's edge storage.
+
+⚠️ This is still a WIP, things such as upload authentication are likely to change.
+
+## Usage
+Maiasaura can either be run on a local Deno runtime or on Deno deploy. The following environment variables are required
+for it to run.
+
+Note: For the time being it only supports one user for uploading artifacts, support for additional users will be added
+in the future.
+
+### Environment Variables
+
+| Variable         | Description                                                                              |
+|------------------|------------------------------------------------------------------------------------------|
+| `BUNNY_ENDPOINT` | The storage endpoint to use. eg: `storage.bunnycdn.com`                                  |
+| `BUNNY_ZONE`     | The name of the storage zone to use. This is similar to a S3 bucket                      |
+| `BUNNY_KEY`      | The storage access key to use                                                            |
+| `BUNNY_CDN`      | The endpoint of your cache pull zone. This is probably `<zone>.b-cdn.net`                |
+| `TITLE`          | The title of your site. This will be in the `title` meta tag as well as on the home page |
+| `USERNAME`       | The username to use for maven artifact uploading                                         |
+| `HASH`           | A BCrypt-hashed password to use for maven artifact uploading                             |
+
+## TODO
+- Purge files from cache on put
+- Database for storing maven users
+- Build in way to generate password hashes
+- Upload generate indexed html files to edge storage
+- Automatic Javadoc expansion
---
+++ main.ts
@@ -0,0 +1,64 @@
+import { compare } from "https://deno.land/x/bcrypt@v0.4.0/mod.ts";
+import { renderFile } from 'https://deno.land/x/mustache@v0.3.0/mod.ts';
+import { router } from "https://crux.land/router@0.0.12";
+import { serve } from "https://deno.land/std@0.146.0/http/server.ts";
+
+import "https://deno.land/std@0.146.0/dotenv/load.ts";
+
+const handler = router({
+    "GET@*": (req) => get(req),
+    "PUT@*": (req) => put(req)
+});
+
+async function get(req: Request): Promise<Response> {
+    let path = new URL(req.url).pathname;
+
+    if (!path.endsWith("/")) {
+        return fetch(`https://${Deno.env.get("BUNNY_CDN") + path}`);
+    }
+
+    const headers = new Headers(req.headers);
+    headers.delete("Referer");
+    headers.append("AccessKey", Deno.env.get("BUNNY_KEY"));
+
+    const resp = await fetch(`https://${Deno.env.get("BUNNY_ENDPOINT")}/${Deno.env.get("BUNNY_ZONE") + path}`, { method: 'GET', headers });
+
+    return index(await resp.json(), path);
+}
+
+async function put(req: Request): Promise<Response> {
+    const authorized = await auth(req.headers);
+
+    if (!authorized) {
+        return new Response("401 – Access denied.");
+    }
+
+    const headers = new Headers(req.headers);
+    headers.append("AccessKey", Deno.env.get("BUNNY_KEY"));
+
+    const url = `https://${Deno.env.get("BUNNY_ENDPOINT")}/${Deno.env.get("BUNNY_ZONE") + new URL(req.url).pathname}`;
+
+    return fetch(url, { method: 'PUT', headers, body: req.body });
+}
+
+async function index(json: any, path: string): Promise<Response> {
+    const directories = json.filter(obj => obj.IsDirectory);
+    const files = json.filter(obj => !obj.IsDirectory);
+
+    const data = { title: Deno.env.get("TITLE"), root: path == "/", path, directories, files };
+
+    return new Response(await renderFile("base.html", data), { headers: { "content-type": "text/html" }});
+}
+
+async function auth(headers: Headers): Promise<boolean> {
+    const authorization = headers.get("Authorization");
+    if (!authorization || !authorization.startsWith("Basic")) {
+        return false;
+    }
+
+    const [username, password] = atob(authorization.slice(6)).split(":");
+
+    return username == Deno.env.get("USERNAME") && await compare(password, Deno.env.get("HASH"));
+}
+
+await serve(handler);
